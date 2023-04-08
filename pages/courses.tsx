44a888c067dbacf5b9bcad37091ffaee797a7bfc
carlos puene <kikeztw@gmail.com>
2023-04-07 21:07:37 -0400

    added tables

--- pages/courses.tsx
+++ pages/courses.tsx
@@ -1,34 +1,66 @@
 import { NextPageWithLayout } from '../src/shared/types/page';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
 
 import { getLayout } from '../src/shared/utils/get-layout';
+import { Table } from '../src/shared/components/table/table';
 
-export const Home: NextPageWithLayout = () => {
+const columns: GridColDef[] = [
+  { field: 'firstName', headerName: 'Nombre', flex: 1 },
+  // {
+  //   field: 'ci',
+  //   headerName: 'Cedula',
+  //   flex: 1,
+  //   editable: false,
+  // },
+  // {
+  //   field: 'email',
+  //   headerName: 'Correo',
+  //   flex: 1,
+  //   editable: false,
+  // },
+  // {
+  //   field: 'grade',
+  //   headerName: 'Nota',
+  //   sortable: false,
+  //   flex: 1,
+  //   // valueGetter: (params: GridValueGetterParams) =>
+  //   //   `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  // },
+];
+
+const data = [
+  { id: 1, lastname: 'Snow', firstName: 'Matematica', grade: 3, email: 'test@test.com', ci: 353423434 },
+  { id: 2, lastname: 'Lannister', firstName: 'Fisica', grade: 4,  email: 'test@test.com', ci: 353423434   },
+  { id: 3, lastname: 'Lannister', firstName: 'Quimica', grade: 4,  email: 'test@test.com', ci: 353423434   },
+  { id: 4, lastname: 'Stark', firstName: 'Castellano', grade: 1,  email: 'test@test.com', ci: 353423434   },
+  // { id: 5, lastname: 'Targaryen', firstName: 'Daenerys', grade: 3,  email: 'test@test.com', ci: 353423434   },
+  // { id: 6, lastname: 'Melisandre', firstName: null, grade: 10,  email: 'test@test.com', ci: 353423434   },
+  // { id: 7, lastname: 'Clifford', firstName: 'Ferrara', grade: 4,  email: 'test@test.com', ci: 353423434   },
+  // { id: 8, lastname: 'Frances', firstName: 'Rossini', grade: 3,  email: 'test@test.com', ci: 353423434   },
+  // { id: 9, lastname: 'Roxie', firstName: 'Harvey', grade: 6,  email: 'test@test.com', ci: 353423434   },
+  // { id: 10, lastname: 'Roxie', firstName: 'Harvey', grade: 6,  email: 'test@test.com', ci: 353423434   },
+  // { id: 92, lastname: 'Roxie', firstName: 'Harvey', grade: 6,  email: 'test@test.com', ci: 353423434   },
+  // { id: 93, lastname: 'Roxie', firstName: 'Harvey', grade: 6,  email: 'test@test.com', ci: 353423434   },
+  // { id: 49, lastname: 'Roxie', firstName: 'Harvey', grade: 6,  email: 'test@test.com', ci: 353423434   },
+  // { id: 39, lastname: 'Roxie', firstName: 'Harvey', grade: 6,  email: 'test@test.com', ci: 353423434   },
+  // { id: 95, lastname: 'Roxie', firstName: 'Harvey', grade: 6,  email: 'test@test.com', ci: 353423434   },
+  // { id: 96, lastname: 'Roxie', firstName: 'Harvey', grade: 6,  email: 'test@test.com', ci: 353423434   },
+
+];
+
+export const CoursesView: NextPageWithLayout = () => {
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-         materias
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Table 
+      title="Materias"
+      columns={columns} 
+      data={data} 
+      button={<Button variant="contained">Registrar Materia</Button>} 
+    />
   );
 }
 
-Home.getLayout = getLayout
+CoursesView.getLayout = getLayout
 
-export default Home;
+export default CoursesView;
--- pages/students.tsx
+++ pages/students.tsx
@@ -1,34 +1,73 @@
 import { NextPageWithLayout } from '../src/shared/types/page';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
 
 import { getLayout } from '../src/shared/utils/get-layout';
+import { Table } from '../src/shared/components/table/table';
 
-export const Home: NextPageWithLayout = () => {
+const columns: GridColDef[] = [
+  { field: 'firstName', headerName: 'Nombre', flex: 1 },
+  { field: 'lastname', headerName: 'Apellido', flex: 1 },
+  {
+    field: 'ci',
+    headerName: 'Cedula',
+    flex: 1,
+    editable: false,
+  },
+  {
+    field: 'phone',
+    headerName: 'Telefono',
+    flex: 1,
+    editable: false,
+  },
+  {
+    field: 'email',
+    headerName: 'Correo',
+    flex: 1,
+    editable: false,
+  },
+  {
+    field: 'status',
+    headerName: 'Estado',
+    sortable: false,
+    flex: 1,
+    // valueGetter: (params: GridValueGetterParams) =>
+    //   `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const data = [
+  { id: 1, lastname: 'Snow', firstName: 'Jon', phone: 353423434, email: 'test@test.com', ci: 353423434 },
+  { id: 2, lastname: 'Lannister', firstName: 'Cersei', phone: 423423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 3, lastname: 'Lannister', firstName: 'Jaime', phone: 453423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 4, lastname: 'Stark', firstName: 'Arya', phone: 163423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 5, lastname: 'Targaryen', firstName: 'Daenerys', phone: 343234234,  email: 'test@test.com', ci: 353423434   },
+  { id: 6, lastname: 'Melisandre', firstName: null, phone: 1534234340,  email: 'test@test.com', ci: 353423434   },
+  { id: 7, lastname: 'Clifford', firstName: 'Ferrara', phone: 443423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 8, lastname: 'Frances', firstName: 'Rossini', phone: 363423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 9, lastname: 'Roxie', firstName: 'Harvey', phone: 653423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 10, lastname: 'Roxie', firstName: 'Harvey', phone: 653423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 92, lastname: 'Roxie', firstName: 'Harvey', phone: 653423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 93, lastname: 'Roxie', firstName: 'Harvey', phone: 653423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 49, lastname: 'Roxie', firstName: 'Harvey', phone: 653423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 39, lastname: 'Roxie', firstName: 'Harvey', phone: 653423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 95, lastname: 'Roxie', firstName: 'Harvey', phone: 653423434,  email: 'test@test.com', ci: 353423434   },
+  { id: 96, lastname: 'Roxie', firstName: 'Harvey', phone: 653423434,  email: 'test@test.com', ci: 353423434   },
+
+];
+
+export const StudenView: NextPageWithLayout = () => {
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-         Estudiantes
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Table 
+      title="Estudiantes"
+      columns={columns} 
+      data={data} 
+      button={<Button variant="contained">Registrar Estudiante</Button>} 
+    />
   );
 }
 
-Home.getLayout = getLayout
+StudenView.getLayout = getLayout
 
-export default Home;
+export default StudenView;
